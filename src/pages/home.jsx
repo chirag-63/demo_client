b437caa6fa082a2e4c70bf8b818669736b545d15
chirag <chiragchauhanx@gmail.com>
2025-01-05 06:45:31 +0530

    added users list

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,20 +1,46 @@
 import { Navbar } from "@/components/navbar"
 import { Input } from "@/components/ui/input"
 import { UserCard } from "@/components/userCard"
+import axios from "axios"
 import { Search } from "lucide-react"
+import { useEffect, useState } from "react"
 
 export default function HomePage() {
+    const [users, setUsers] = useState([]);
+    const [loading, setLoading] = useState(true)
+    const [filter, setFilter] = useState('');
+
+    useEffect(() => {
+        const getAllUsers = async () => {
+            setLoading(true)
+            try {
+                const result = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/all?filter=${filter}`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                })
+                setUsers(result.data.user)
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        getAllUsers();
+    }, [filter])
+
+    const dummyUser = {
+        _id: 'dummy-id',
+        name: 'Loading User',
+    };
+
     return (
         <>
             <Navbar />
-            <div className="mx-2 lg:mx-2 px-2 gap-5 items-center mt-6">
-                <h1 className="text-xl font-serif font-semibold text-center">
-                    Welcome Chirag, what a beautiful day..
-                </h1>
-            </div>
-            <div className=" mx-2 lg:mx-40 gap-10 flex flex-col items-center px-2">
+            <div className=" mx-2 mt-8 lg:mx-40 gap-10 flex flex-col items-center px-2">
                 <div className="w-full lg:px-32 ">
                     <Input
+                        onChange={(e) => setFilter(e.target.value)}
                         style={{ fontSize: '16px' }}
                         className="h-10 pl-10 md:h-12 mt-4"
                         placeholder="find new friends, for life..."
@@ -26,18 +52,22 @@ export default function HomePage() {
 
                 <div className="w-full mb-10 grid gap-4 items-center 
                     sm:grid-cols-1 lg:grid-cols-2">
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
-                    <UserCard text={"Add"} color={"blue"} />
+                    {loading
+                        ? (
+                            Array(4).fill().map((_, index) => (
+                                <UserCard
+                                    key={index}
+                                    user={dummyUser}
+                                    text={"Add"}
+                                />
+                            ))
+                        )
+                        : (users.map(
+                            user => <UserCard
+                                key={user._id} text={"Add"} user={user}
+                            />)
+                        )
+                    }
                 </div>
             </div>
         </>
--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -2,8 +2,68 @@ import { Navbar } from "@/components/navbar"
 import { Input } from "@/components/ui/input"
 import { UserCard } from "@/components/userCard"
 import { Search } from "lucide-react"
+import { useEffect, useState } from "react"
+import axios from "axios"
 
 export default function ProfilePage() {
+    const [friends, setFriends] = useState([]);
+    const [user, setUser] = useState({});
+    const [filter, setFilter] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [friendCount, setFriendCount] = useState(0)
+
+    useEffect(() => {
+        const getFriendsList = async () => {
+            setLoading(true)
+            try {
+                const result = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/profile/friends`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                })
+                setFriends(result.data)
+            } catch (error) {
+                console.error(error);
+            } finally {
+                setLoading(false)
+            }
+        }
+        getFriendsList();
+    }, [])
+
+    useEffect(() => {
+        const getMyProfile = async () => {
+            try {
+                const result = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/profile/me`, {
+                    headers: {
+                        Authorization: `Bearer ${localStorage.getItem('token')}`
+                    }
+                })
+                console.log(result.data)
+                setUser(result.data.user)
+                setFriendCount(result.data.friendCount)
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        getMyProfile();
+    }, [])
+
+    const handleRemoveFriend = (userId) => {
+        setFriends((prev) => prev.filter((user) => user._id !== userId));
+    };
+
+    const filteredFriends = friends.filter(
+        (friend) =>
+            friend.name.toLowerCase().includes(filter.toLowerCase()) ||
+            friend.username.toLowerCase().includes(filter.toLowerCase())
+    );
+
+    const dummyUser = {
+        _id: 'dummy-id',
+        name: 'Loading User',
+    };
+
     return (
         <>
             <Navbar />
@@ -16,13 +76,13 @@ export default function ProfilePage() {
                 />
                 <div className="my-3 py-2 ml-5">
                     <div className="text-2xl font-semibold">
-                        Chirag Chauhan
+                        {user.name}
                     </div>
                     <div className="text-base italic ">
-                        @chirag3199
+                        @{user.username}
                     </div>
                     <div className="mt-2">
-                        Total Friends - 10
+                        Total Friends - {friendCount}
                     </div>
                 </div>
             </div>
@@ -30,8 +90,10 @@ export default function ProfilePage() {
                 <div className="w-full lg:px-32 lg:mt-4">
                     <Input
                         style={{ fontSize: '16px' }}
+                        value={filter}
                         className="h-10 pl-10 md:h-12 mt-8"
                         placeholder="Search among your friends..."
+                        onChange={(e) => setFilter(e.target.value)}
                     />
                     <Search
                         className="ml-2 h-4 md:h-5 -mt-7 md:-mt-8 "
@@ -39,15 +101,27 @@ export default function ProfilePage() {
                 </div>
                 <div className="w-full mb-10 grid gap-4 items-center 
                     sm:grid-cols-1 lg:grid-cols-2">
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
-                    <UserCard text={"Remove"} color={"red"} />
+                    {loading
+                        ? (Array(4).fill().map((_, index) => (
+                            <UserCard
+                                key={index}
+                                user={dummyUser}
+                                text={"remove"}
+                            />
+                        ))
+                        ) : (
+                            filteredFriends.map(
+                                (friend) => (
+                                    <UserCard
+                                        key={friend._id}
+                                        text={"Remove"}
+                                        user={friend}
+                                        onRequestHandled={handleRemoveFriend}
+                                    />
+                                )
+                            )
+                        )
+                    }
                 </div>
             </div>
         </>
