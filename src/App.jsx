365214fb554662029506669de0cfb7c068f2011d
chirag <chiragchauhanx@gmail.com>
2025-01-05 17:53:23 +0530

    file name bug

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignupPage from "./pages/signup"
 import LoginPage from "./pages/login"
-import ProtectedRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/protectedRoute';
 import HomePage from "./pages/home";
 import ProfilePage from "./pages/profile";
 
